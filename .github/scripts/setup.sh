6a042c8e06937358a2a927bc168f9b1075e81f7b
Michka Popoff <michkapopoff@gmail.com>
2021-05-11 23:47:45 +0200

    workflow: download scripts

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -24,53 +24,19 @@ if [ ! -d "/home/actions" ]; then
     newgrp docker
     systemctl restart docker
 
-    cat > install_runner.sh <<'endmsg'
-#!/bin/bash
-set -x
-
-mkdir -p /home/actions/actions-runner
-curl https://api.github.com/repos/actions/runner/releases/latest |
-  egrep -o 'https://github.com/actions/runner/releases/download/v[0-9.]+/actions-runner-linux-x64-[0-9.]+\.tar\.gz' |
-  uniq |
-  xargs curl -L |
-  tar xzv -C /home/actions/actions-runner
-endmsg
-
-  # This needs to be run with the actions user:
-  chmod +x install_runner.sh
-  su -c "./install_runner.sh" actions
-
-  # This needs to be run as root:
-  source /home/actions/actions-runner/bin/installdependencies.sh
-
-    cat > config_runner.sh <<'endmsg'
-#!/bin/bash
-set -x
-
-cd /home/actions/actions-runner
-./config.sh \
-  --url "https://github.com/Homebrew/homebrew-core" \
-  --token "${VM_TOKEN}" \
-  --name "${RUNNER_NAME}" \
-  --labels ${RUNNER_NAME} \
-  --replace \
-  --unattended \
-  --work _work
-endmsg
+    curl -o install_runner.sh https://raw.githubusercontent.com/Homebrew/actions/master/create-gcloud-instance/install_runner.sh
+    curl -o config_runner.sh https://raw.githubusercontent.com/Homebrew/actions/master/create-gcloud-instance/config_runner.sh
 
     # This needs to be run with the actions user:
-    chmod +x config_runner.sh
+    su -c "./install_runner.sh" actions
+
+    # This needs to be run as root:
+    source /home/actions/actions-runner/bin/installdependencies.sh
+
+    # This needs to be run with the actions user:
     RUNNER_NAME=$RUNNER_NAME VM_TOKEN=$VM_TOKEN su -c "./config_runner.sh" actions
 fi
 
-cat > start_runner.sh <<'endmsg'
-#!/bin/bash
-set -x
-
-cd /home/actions/actions-runner
-./bin/runsvc.sh
-endmsg
-
-chmod +x start_runner.sh
+curl -o start_runner.sh https://raw.githubusercontent.com/Homebrew/actions/master/create-gcloud-instance/start_runner.sh
 
 su -p -c "./start_runner.sh" actions
